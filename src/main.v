1cb6833c726f7126b27f51e0c08de8fdb977f94c
ivone <ivone@ivone.lol>
2023-05-16 11:47:32 +0200

    feat: :sparkles: allow inline comments

--- src/main.v
+++ src/main.v
@@ -76,7 +76,7 @@ fn parse_program(lines []string) !Program {
 		if line == '' {
 			continue
 		}
-		tokens := line.split_any(' \t').filter(it != '')
+		tokens := line.split_nth(';', 2)[0].split_any(' \t').filter(it != '')
 		if tokens.len > 3 {
 			return error('too many tokens in line ${i + 1}')
 		}
